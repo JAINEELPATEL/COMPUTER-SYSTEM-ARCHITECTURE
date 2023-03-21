13ba61923da43bc43e9468e7ad7a16a744d73bb2
JAINEEL PATEL <95839179+JAINEELPATEL@users.noreply.github.com>
2023-03-21 19:11:29 +0530

    Update 5 JK FLIP FLOP DIRECT.py

--- 5 JK FLIP FLOP DIRECT.py
+++ 5 JK FLIP FLOP DIRECT.py
@@ -1,10 +1,20 @@
 #IMPLEMENTATION OF JK FLIP FLOP - DIRECT METHOD
-J = int(input('Value for j : '))
-K = int(input('Value for k : '))
+
+#Take Input
+J = int(input('Value for J : '))
+K = int(input('Value for K : '))
 Qp = int(input('Value for Qp(Previous o/p) : '))
-
-#Qn=J Qp' + K' Qp
-#Qn=OR(AND(J,NOT(Qp)),AND(NOT(K),Qp))
+#Calculate Qn=J Qp' + K' Qp
 Qn=J&(~Qp+2)|(~K+2)&Qp
-
+#Print Output (Next State)
+print()
 print('Output Qn : ',Qn)
+#Print State
+if J==0 and K==0 :
+    print('Hold State')
+elif J==0 and K==1 :
+    print('Reset State')
+elif J==1 and K==0 :
+    print('Set State')
+elif J==1 and K==1 :
+    print('Toggle State')
